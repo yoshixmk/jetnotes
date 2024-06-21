3a86dcd69f0a9d59dfd58b9023f242dbf610024d
yoshixmk <pirori67@gmail.com>
2024-06-21 22:16:31 +0900

    pass args on NoteColor

--- app/src/main/java/com/yourcompany/android/jetnotes/ui/components/NoteColor.kt
+++ app/src/main/java/com/yourcompany/android/jetnotes/ui/components/NoteColor.kt
@@ -1,28 +1,51 @@
 package com.yourcompany.android.jetnotes.ui.components
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun NoteColor() {
+fun NoteColor(
+  color: Color,
+  size: Dp,
+  padding: Dp = 0.dp,
+  border: Dp,
+) {
   Box(
     modifier = Modifier
-      .size(40.dp)
+      .padding(padding)
+      .size(size)
       .clip(CircleShape)
-      .background(Color.Red)
+      .background(color)
+      .border(
+        BorderStroke(
+          border,
+          SolidColor(Color.Black)
+        ),
+        CircleShape
+      )
   )
 }
 
 @Preview
 @Composable
 fun NoteColorPreview() {
-  NoteColor()
+  NoteColor(
+    color = Color.Red,
+    size = 40.dp,
+    padding = 4.dp,
+    border = 2.dp
+  )
 }
