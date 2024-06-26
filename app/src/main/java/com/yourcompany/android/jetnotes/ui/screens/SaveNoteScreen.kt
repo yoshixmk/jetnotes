58a253283d9aeb3d0783dbea1822813e059ec320
yoshixmk <pirori67@gmail.com>
2024-06-26 10:04:08 +0900

    crate save note screen and implement color picker

---
+++ app/src/main/java/com/yourcompany/android/jetnotes/ui/screens/SaveNoteScreen.kt
@@ -0,0 +1,90 @@
+package com.yourcompany.android.jetnotes.ui.screens
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.yourcompany.android.jetnotes.domain.model.ColorModel
+import com.yourcompany.android.jetnotes.ui.components.NoteColor
+import com.yourcompany.android.jetnotes.util.fromHex
+
+@Composable
+private fun ColorPicker(
+  colors: List<ColorModel>,
+  onColorSelect: (ColorModel) -> Unit,
+) {
+  Column(modifier = Modifier.fillMaxWidth()) {
+    Text(
+      text = "Color picker",
+      fontSize = 18.sp,
+      fontWeight = FontWeight.Bold,
+      modifier = Modifier.padding(8.dp)
+    )
+    LazyColumn(modifier = Modifier.fillMaxWidth()) {
+      items(colors.size) { itemIndex ->
+        val color = colors[itemIndex]
+        ColorItem(
+          color = color,
+          onColorSelect = onColorSelect
+        )
+      }
+    }
+  }
+}
+
+@Preview
+@Composable
+fun ColorPickerPreview() {
+  ColorPicker(
+    colors = listOf(
+      ColorModel.DEFAULT,
+      ColorModel.DEFAULT,
+      ColorModel.DEFAULT
+    ) ){}
+}
+
+@Composable
+fun ColorItem(
+  color: ColorModel,
+  onColorSelect: (ColorModel) -> Unit,
+) {
+  Row(
+    modifier = Modifier
+      .fillMaxWidth()
+      .clickable(
+        onClick = {
+          onColorSelect(color)
+        }
+      )) {
+    NoteColor(
+      modifier = Modifier.padding(10.dp),
+      color = Color.fromHex(color.hex),
+      size = 80.dp,
+      border = 2.dp
+    )
+    Text(
+      text = color.name,
+      fontSize = 22.sp,
+      modifier = Modifier
+        .padding(horizontal = 16.dp)
+        .align(Alignment.CenterVertically)
+    )
+  }
+}
+
+@Preview
+@Composable
+fun ColorItemPreview() {
+  ColorItem(ColorModel.DEFAULT) {}
+}
