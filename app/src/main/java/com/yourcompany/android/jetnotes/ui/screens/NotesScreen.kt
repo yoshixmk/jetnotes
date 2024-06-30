bbc2a46b7e22a286c66c4f1dfd564dbe086e9bcd
yoshixmk <pirori67@gmail.com>
2024-06-30 14:25:39 +0900

    create Note Screen

--- app/src/main/java/com/yourcompany/android/jetnotes/ui/screens/NotesScreen.kt
+++ app/src/main/java/com/yourcompany/android/jetnotes/ui/screens/NotesScreen.kt
@@ -1,9 +1,41 @@
 package com.yourcompany.android.jetnotes.ui.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.List
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import com.yourcompany.android.jetnotes.domain.model.NoteModel
+import com.yourcompany.android.jetnotes.ui.components.Note
+import com.yourcompany.android.jetnotes.ui.components.TopAppBar
 import com.yourcompany.android.jetnotes.viewmodel.MainViewModel
 
 @Composable
 fun NotesScreen(viewModel: MainViewModel) {
-
+  val notes: List<NoteModel> by viewModel
+    .notesNotInTrash
+    .observeAsState(listOf())
+  Column {
+    TopAppBar(
+      title = "JetNotes",
+      icon = Icons.AutoMirrored.Filled.List,
+      onIconClick = {}
+    )
+    LazyColumn {
+      items(count = notes.size) { noteIndex ->
+        val note = notes[noteIndex]
+        Note(
+          note = note,
+          onNoteClick = {
+            viewModel.onNoteClick(it)
+          },
+          onNoteCheckedChange = {
+            viewModel.onNoteCheckedChange(it)
+          }
+        )
+      }
+    }
+  }
 }
