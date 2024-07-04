204f3a46bb64eb4f261455025bde087e4c26402c
yoshixmk <pirori67@gmail.com>
2024-07-04 10:07:45 +0900

    refactor: create NotesList

--- app/src/main/java/com/yourcompany/android/jetnotes/ui/screens/NotesScreen.kt
+++ app/src/main/java/com/yourcompany/android/jetnotes/ui/screens/NotesScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.material.icons.automirrored.filled.List
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.tooling.preview.Preview
 import com.yourcompany.android.jetnotes.domain.model.NoteModel
 import com.yourcompany.android.jetnotes.ui.components.Note
 import com.yourcompany.android.jetnotes.ui.components.TopAppBar
@@ -23,19 +24,42 @@ fun NotesScreen(viewModel: MainViewModel) {
       icon = Icons.AutoMirrored.Filled.List,
       onIconClick = {}
     )
-    LazyColumn {
-      items(count = notes.size) { noteIndex ->
-        val note = notes[noteIndex]
-        Note(
-          note = note,
-          onNoteClick = {
-            viewModel.onNoteClick(it)
-          },
-          onNoteCheckedChange = {
-            viewModel.onNoteCheckedChange(it)
-          }
-        )
-      }
+    NotesList(
+      notes = notes,
+      onNoteCheckedChange = { viewModel.onNoteCheckedChange(it) },
+      onNoteClick = { viewModel.onNoteClick(it) }
+    )
+  }
+}
+
+@Composable
+private fun NotesList(
+  notes: List<NoteModel>,
+  onNoteCheckedChange: (NoteModel) -> Unit,
+  onNoteClick: (NoteModel) -> Unit,
+) {
+  LazyColumn {
+    items(count = notes.size) { noteIndex ->
+      val note = notes[noteIndex]
+      Note(
+        note = note,
+        onNoteClick = onNoteClick,
+        onNoteCheckedChange = onNoteCheckedChange
+      )
     }
   }
+}
+
+@Preview
+@Composable
+private fun NotesListPreview() {
+  NotesList(
+    notes = listOf(
+      NoteModel(1, "Note 1", "Content 1", null),
+      NoteModel(2, "Note 2", "Content 2", false),
+      NoteModel(3, "Note 3", "Content 3", true)
+    ),
+    onNoteCheckedChange = {},
+    onNoteClick = {}
+  )
 }
